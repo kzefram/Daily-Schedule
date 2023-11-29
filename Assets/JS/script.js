b31c6e3d8cef37b6dde05bef1815a9713f528c77
KarenB <karenabourgeois@gmail.com>
2023-11-29 11:53:39 -0400

    Added the current date and time

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
+document.getElementById("currentDay").innerHTML = Date();
+
+		var currentDate = new Date();
+
+		document.getElementById("currentDay").innerHTML = currentDate;
+    
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -21,4 +30,5 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+
   
