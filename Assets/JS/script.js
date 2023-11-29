396e7890b35bfaaaf491ae335830c059511e3135
KarenB <karenabourgeois@gmail.com>
2023-11-29 14:36:59 -0400

    Added some Javascript

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-
-
-document.getElementById("currentDay").innerHTML = Date();
-
-		var currentDate = new Date();
-
-		document.getElementById("currentDay").innerHTML = currentDate;
-    
+   
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -27,8 +19,17 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
+
+      
+
     //
     // TODO: Add code to display the current date in the header of the page.
+
+    document.getElementById("currentDay").innerHTML = Date();
+
+		var currentDate = new Date();
+
+		document.getElementById("currentDay").innerHTML = currentDate;
   });
 
   
