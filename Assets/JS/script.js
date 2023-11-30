b82bec13986cbb900b8477c16bd59a06323f50e4
KarenB <karenabourgeois@gmail.com>
2023-11-30 14:21:25 -0400

    Moved current date code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,11 +4,7 @@
 
 
 
-document.getElementById("currentDay").innerHTML = Date();
 
-		var currentDate = new Date();
-
-		document.getElementById("currentDay").innerHTML = currentDate;
     
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
@@ -27,8 +23,14 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
+    
     //
     // TODO: Add code to display the current date in the header of the page.
+    document.getElementById("currentDay").innerHTML = Date();
+
+		var currentDate = new Date();
+
+		document.getElementById("currentDay").innerHTML = currentDate;
   });
 
   
